8291d9624c7e79e1f1e8a1b9d375798aef811703
Tanner <tannerohio@gmail.com>
2025-01-03 10:53:23 -0500

    main

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from javascript import require, On, off
 
+#GLOBAL VAR
+reconnect = True
 
+#Bot
 mineflayer = require('mineflayer')
 
 bot_args =     {
@@ -12,29 +15,44 @@ bot_args =     {
     }
 bot = mineflayer.createBot(bot_args)
 
-@On(bot, "login")
-def login(this):
-    bot_socket = bot._client.socket
-    print(f"Logged in to {bot_socket.server if bot_socket.server else bot_socket.host}")
 
-@On(bot, "kicked")
-def kicked(this, reason, loggedIn):
-    if loggedIn:
-        print(f"Kicked from server: {reason}")
-    else:
-        print(f"Kicked from server while connecting: {reason}")
+#Event Listeners
+def start_bot():
+    @On(bot, "login")
+    def login(this):
+        bot_socket = bot._client.socket
+        print(f"Logged in to {bot_socket.server if bot_socket.server else bot_socket.host}")
 
-@On(bot, "messagestr")
-def messagestr(this, message, messageposition):
-    if messageposition == "chat" and "quit" in message:
-        this.quit()
+    @On(bot, "kicked")
+    def kicked(this, reason, loggedIn):
+        if loggedIn:
+            print(f"Kicked from server: {reason}")
+        else:
+            print(f"Kicked from server while connecting: {reason}")
 
-@On(bot, "end")
-def end(this, reason):
-    print(f"Disconnected: {reason}")
+    @On(bot, "messagestr")
+    def messagestr(this, message, messageposition):
+        if messageposition == "chat" and "quit" in message:
+            global reconnect
+            reconnect = False
+            this.quit()
 
-    #Turning Off listeners
-    off(bot, "login",login)
-    off(bot, "kicked", kicked)
-    off(bot, "end", end)
-    off(bot, "messagestr", messagestr)
+
+    # Ending 
+    @On(bot, "end")
+    def end(this, reason):
+        print(f"Disconnected: {reason}")
+
+        #Turning Off listeners
+        off(bot, "login",login)
+        off(bot, "kicked", kicked)
+        off(bot, "messagestr", messagestr)
+        if reconnect:
+            print("RESTARTING")
+            start_bot()
+
+        off(bot, "end", end)
+
+#start call
+start_bot()
+    
